0e276f6c3a0897242ad9a873b941548892a074ac
Angel Febles <licenciadofebles@gmail.com>
2023-11-29 01:35:39 -0600

    Added event listeners for inputs

--- rainValues.js
+++ rainValues.js
@@ -1,4 +1,55 @@
-var numraindrops="100";
-var speed="2";
-var rainsize="3";
-var wind="no";
+let ammountSlide = document.getElementById("numraindropsID");
+let speedSlide= document.getElementById("speedID");
+let sizeSlide = document.getElementById("rainSizeID");
+
+let windLeft = document.getElementById("switch-left");
+let windNo = document.getElementById("switch-no");
+let windRight = document.getElementById("switch-right");
+
+//Default rain propreties
+let numraindrops= ammountSlide.value;
+let speed= speedSlide.value;
+let rainsize=sizeSlide.value;
+let wind="no";
+
+
+
+ammountSlide.addEventListener("change", function(){
+    numraindrops = parseInt(ammountSlide.value);
+    //console.log(parseInt(numraindrops);
+
+});
+
+speedSlide.addEventListener("change", function(){
+    speed = parseInt(speedSlide.value);
+    //console.log(speed);
+});
+
+sizeSlide.addEventListener("change", function(){
+    rainsize = parseInt(sizeSlide.value);
+    //console.log(rainsize);
+});
+
+//Wind switch event listeners
+windLeft.addEventListener("change", function() {
+    if(this.checked){
+        wind = "left";
+        //console.log(wind);
+
+    }
+});
+
+windNo.addEventListener("change", function() {
+    if(this.checked){
+        wind = "no";
+        //console.log(wind);
+    }
+
+});
+
+windRight.addEventListener("change", function() {
+    if(this.checked){
+        wind = "right";
+        //console.log(wind);
+    }
+});
